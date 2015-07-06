35975b60272d0bfccd3e93e1d559abd22b8d5607
John Lowe <jblowe@berkeley.edu>
2015-07-05 17:38:45 -0700

    NOJIRA: get rid of some middleware includes

--- cspace_django_site/settings.py
+++ cspace_django_site/settings.py
@@ -126,7 +126,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'mobileesp.middleware.MobileDetectionMiddleware',
+    #'mobileesp.middleware.MobileDetectionMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
